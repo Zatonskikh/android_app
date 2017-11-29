0554a920ae9af924abbb958fbeb07282ad262666
CORP\ZatonskihEV <zatonskihEV@taxcom.ru>
2017-11-29 23:56:49 +0300

    Обновил гит игнор

--- app/src/main/java/com/example/zatonskihev/singlepageapp/MainViewHolder.java
+++ app/src/main/java/com/example/zatonskihev/singlepageapp/MainViewHolder.java
@@ -1,5 +1,6 @@
 package com.example.zatonskihev.singlepageapp;
 
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -7,8 +8,12 @@ public class MainActivityViewHolder {
 
         public final EditText editText;
         public final TextView textView;
+        public final Button submitText;
+        public final Button resetEditText;
+        public final Button resetTextView;
 
-        public MainActivityViewHolder(EditText editText, TextView textView) {
+
+        public MainActivityViewHolder(EditText editText, TextView textView, Button) {
             this.editText = editText;
             this.textView = textView;}
 }
